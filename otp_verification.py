244b70e9f8ba99b874f098575b1fa3da2d55dc81
AmitBoricha <borichaamit1@gmail.com>
2023-09-19 18:45:07 +0530

    added

--- otp_verification.py
+++ otp_verification.py
@@ -17,3 +17,8 @@ s.login("Your Gmail Account", "You app password")
 emailid = input("Enter your email: ")
 s.sendmail('&&&&&&&&&&&',emailid,msg)
 a = input("Enter Your OTP >>: ")
+
+if a == OTP:
+    print("Verified")
+else:
+    print("Please Check your OTP again")
